373f2441356ea34b8ef5660e962a8d83640b0f91
Anant Shrivastava <anant@anantshri.info>
2016-03-10 13:44:06 +0530

    Added files via upload

---
+++ Fuzzing/Anto/exec_me.sh
@@ -0,0 +1,14 @@
+#!/system/bin/sh
+export SHELL=/system/bin/sh
+export MKSH=/system/bin/sh
+export PWD=/system/bin/
+cd $PWD
+
+pwd
+echo $SHELL
+echo $MKSH
+echo "uid is ${UID}"
+echo "user is ${USER}"
+echo "username is ${USERNAME}"
+
+pngtest 
